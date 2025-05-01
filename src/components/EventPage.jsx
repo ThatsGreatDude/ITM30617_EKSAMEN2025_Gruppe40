b0248dd33a0c30c080bc50235c7b302030d5e174
AndersKlevaas1 <anderkl@hiof.no>
2025-05-01 14:16:01 +0200

    lagt inn routing for eventpage
    Det har nå blitt lagt inn routing fra eventpagen til Hjem

--- src/components/EventPage.jsx
+++ src/components/EventPage.jsx
@@ -1,21 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-const { events_id } = useParams();
-const [events, setEvents] = useState();
+const API_KEY = "AgNENsWPtsr9hDbDVE6OHkBjGeHHc20W";
 
-const getEvents = async () => {
-  fetch(`https://app.ticketmaster.com/discovery/v2/events/${id}.json?apikey=${API_KEY}`)
-    .then((response) => response.json())
-    .then((data) => setEvents(data.data))
-    .catch((error) =>
-      console.error("det skjedde en feil under fecth", error)
-    );
-};
+function EventPage() {
+  const { events_id } = useParams();
+  const [events, setEvents] = useState();
 
-useEffect(() => {
-  getEvents);
-}, [events_id]);
-return <h1>{events?.name}</h1>;
+  const getEvents = async () => {
+    fetch(`https://app.ticketmaster.com/discovery/v2/events/${events_id}.json?apikey=${API_KEY}`)
+      .then((response) => response.json())
+      .then((data) => setEvents(data))
+      .catch((error) =>
+        console.error("det skjedde en feil under fecth", error)
+      );
+  };
+
+  useEffect(() => {
+    getEvents();
+  }, [events_id]);
+
+  return <h1>{events?.name}</h1>;
 }
- 
+
+export default EventPage;
