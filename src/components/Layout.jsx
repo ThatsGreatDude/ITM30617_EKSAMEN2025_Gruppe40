0edb8d61e8e3d63d69df0c681e4665937d7e35cc
maliharon <142988041+maliharon@users.noreply.github.com>
2025-05-01 13:22:53 +0200

    hei, jeg laget Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,2 +1,18 @@
+import {Link} from "react-router-dom";
 
 
+export default function Layout({children}){
+    return(
+            <>
+            <header>
+                <nav>
+                    <link to="/"></link>
+                    <link to="Musikk">Musikk</link>
+                    <link to="Sport">sport</link>
+                    <link to="Teater/Show">Teater/show</link>
+                </nav>
+
+            </header>
+            </>
+    );
+}
