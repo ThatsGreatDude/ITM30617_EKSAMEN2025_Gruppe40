41b77314ba748fada4016638df6a401048ec52ed
ThatsGreatDude <malenest@hiof.no>
2025-05-01 14:03:14 +0200

    api osv
    Fylte ut filler funksjonene

--- src/components/EventPage.jsx
+++ src/components/EventPage.jsx
@@ -1,22 +1,21 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
-const { filler_id } = useParams();
-const [filler, setFiller] = useState();
+const { events_id } = useParams();
+const [events, setEvents] = useState();
 
-const getFiller = async () => {
-  fetch(``) {/* Må huske å legge til den egentlige URLen -malene */}
+const getEvents = async () => {
+  fetch(`https://app.ticketmaster.com/discovery/v2/events/${id}.json?apikey=${API_KEY}`)
     .then((response) => response.json())
-    .then((data) => setFiller(data.data))
+    .then((data) => setEvents(data.data))
     .catch((error) =>
       console.error("det skjedde en feil under fecth", error)
     );
 };
 
 useEffect(() => {
-  getFiller();
-}, [filler_id]);
-return <h1>{filler?.name}</h1>;
+  getEvents);
+}, [events_id]);
+return <h1>{events?.name}</h1>;
 }
  
-{/* Filler står som foreløpig filler name for hva det egentlig skal kalles - malene*/}
